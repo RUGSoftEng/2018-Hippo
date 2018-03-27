8476a9111d0c49765c2c5148c8fb32621ecda52f
SardorKH <s.khashimov@student.rug.nl>
2018-03-27 12:53:42 +0200

    Added style elements

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,5 +1,42 @@
 Vue.component('nav-bar', {
   template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Hippo<!--<img src="Assets/Hippo-Poster.png" alt="" width="100"> --></a>
+        <div class="container">
+          <form class="form-inline">
+            <input class="form-control mr-sm-3" type="search" placeholder="Search for tweets..." aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+
+
+  <!--
     <div class="container">
       <nav class="navbar navbar-light">
         <a class="navbar-brand" href="#">
@@ -13,15 +50,43 @@ Vue.component('nav-bar', {
         </form>
       </nav>
     </div>
+    -->
+  `
+})
+
+Vue.component('logo-poster', {
+  template: `
+    <header class="business-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <h1 class="display-3 text-center text-white mt-4"></h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  `
+})
+
+Vue.component('search-bar', {
+  template: `
+    <div class="container center">
+      <form class="form-inline">
+        <input class="form-control mr-sm-3" type="search" placeholder="Search for tweets..." aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
   `
 })
 
 Vue.component('tweet-list', {
   props: ['tweetList'],
   template:`
-    <div class="container text-center">
-      <tweet v-for="t in tweetList" :tweetData=t></tweet>
-    </div>
+    <section class="py-5">
+      <div class="container text-center">
+        <tweet v-for="t in tweetList" :tweetData=t></tweet>
+      </div>
+    </section>
   `
 })
 
@@ -47,7 +112,12 @@ Vue.component('layout', {
 
   template:`
     <div>
-      <nav-bar></nav-bar>
+      <!-- <nav-bar></nav-bar> -->
+      <logo-poster></logo-poster>
+      <center>
+        <h1 id="tweetTitle">Tweets</h1>
+        <search-bar></search-bar>
+      </center>
       <tweet-list :tweetList="$data.tweetList"></tweet-list>
     </div>
   `
