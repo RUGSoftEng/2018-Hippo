635fbfdf8e15d59d3546c464f7ed706c6be7498e
LeviUni <l.a.van.rheenen@student.rug.nl>
2018-03-29 15:04:21 +0200

    Got tweets from internet to replace placeholder tweets

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,9 @@
 from flask import *
 import json
 import sys
+from decimal import Decimal
 
-placeholder=[{'tweet-id': "ABC", 'content': "Hello world!", 'keywords': ["hello","world"]}]
+placeholder=[]
 
 app = Flask(__name__)
 
@@ -15,5 +16,16 @@ def search(term):
     res = [res for res in placeholder if res['keywords'].count(term)>=1]
     return jsonify(res)
 
+def setup_placeholder():
+	tweets_file = open('tweets.txt', 'r')
+	
+	for line in tweets_file:
+		arr = line.split('\t')
+		tweet_id = str(int(Decimal(arr[0].replace('\"', ''))))
+		tweet_contents = arr[2].replace('\"', '')
+		tweet_keywords = tweet_contents.split(' ')
+		placeholder.append({'tweet-id':tweet_id, 'content':tweet_contents, 'keywords':tweet_keywords})
+	
 if __name__ == '__main__':
+	setup_placeholder()
     app.run(debug=True)
