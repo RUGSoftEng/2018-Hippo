484665156debe943fa03c8658b79d6283eb1112c
Hydro7 <said.faroghi@gmail.com>
2018-05-01 20:59:15 +0400

    Removed previous non-webpack directory

--- Frontend/frontend-webpack/src/main.js
+++ Frontend/frontend-webpack/src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+/*jshint esversion: 6 */
+import Vue from 'vue';
+import BootstrapVue from "bootstrap-vue";
+import App from './App.vue';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
