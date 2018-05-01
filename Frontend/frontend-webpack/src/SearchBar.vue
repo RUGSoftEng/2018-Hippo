39cba015d7438eca4e595342a10229e4f4309442
Hydro7 <said.faroghi@gmail.com>
2018-05-01 20:31:47 +0400

    Axios installed

--- Frontend/frontend-webpack/src/SearchBar.vue
+++ Frontend/frontend-webpack/src/SearchBar.vue
@@ -9,7 +9,29 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  data() {
+    return {
+      searchText: '',
+      tweetList: []
+    };
+  },
+  methods:{
+    search: function(term) {
+      // this.$data.tweetList = [{'tweet-id': "ABC", 'content': "Hello world!", 'keywords': ["hello","world"]},
+      //        {'tweet-id': "BBC", 'content': "Hello earth!", 'keywords': ["hello","earth"]},
+      //        {'tweet-id': "CBC", 'content': "These tweets are placeholders because there is no backend yet as of this pull request!", 'keywords': ["hello","universe"]}];
+      let componentContext = this;
+      axios.get('http://localhost:5000/search/' + term)
+        .then(function (response) {
+          componentContext.$data.tweetList = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+       });
+    }
+  }
 }
 </script>
 
