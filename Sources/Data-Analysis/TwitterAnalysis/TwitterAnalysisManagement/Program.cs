70f4a4a62f2efef280afc4599b61b3414d988226
Jean P.D. Meijer <9619050+JeanMeijer@users.noreply.github.com>
2018-05-11 16:35:37 +0200

    End of sprint commit. (#32)
    * simple tokenization + pos-tagging
    
    * named entity reconition, grammar
    
    * extracting synonyms
    
    * Initial docker configuration.
    
    * Added initial code data streamer.
    
    OAuth tokens should not be in a public repository.
    
    * Added Ignite and Elasticsearch
    
    * Some refactoring.
    
    * Removed Ignite from the web API.
    
    * Docker config for Flask.
    
    * Added Maven config.
    
    * Fixed typo.
    
    * Added Docker config for Maven.
    
    * saveee
    
    * put to json
    
    * elastic search thingy
    
    * Rewrote data streamer in C#.
    
    * Updated time tracking Jean.

---
+++ Sources/Data-Analysis/TwitterAnalysis/TwitterAnalysisManagement/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+using TwitterRealtimeAPI;
+
+namespace TwitterAnalysisManagement
+{
+    public class Program
+    {
+        public static TwitterDataStreamer DataStreamer { get; set; }
+        
+        public static void Main(string[] args)
+        {
+            DataStreamer = new TwitterDataStreamer();
+            
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
