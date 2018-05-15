a1e7ab3e28b4490b2456b1b16f67c9b182b0ae92
Hydro7 <said.faroghi@gmail.com>
2018-05-15 15:46:46 +0400

    infiniteScroll

--- Frontend/frontend-webpack/src/SearchBar.vue
+++ Frontend/frontend-webpack/src/SearchBar.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class="container center">
     <div class="form-inline">
-      <input class="form-control mr-sm-3" type="text" v-model="searchText" placeholder="Search for tweets..." aria-label="Search">
+      <input class="form-control mr-sm-3" type="text" v-model="searchText" v-on:keyup.  enter="search(searchText)" placeholder="Search for tweets..." aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" v-on:click="search(searchText)">Search</button>
     </div>
     <tweet-list :tweetList="tweetList"></tweet-list>
@@ -15,26 +15,58 @@ export default {
   data() {
     return {
       searchText: '',
-      tweetList: []
+      tweetList: [],
+      startIndex: 0,
+      endIndex: 9,
+      newTweetFactor: 10,
     };
   },
   methods:{
     search: function(term) {
-      // this.$data.tweetList = [{'tweet-id': "ABC", 'content': "Hello world!", 'keywords': ["hello","world"]},
-      //        {'tweet-id': "BBC", 'content': "Hello earth!", 'keywords': ["hello","earth"]},
-      //        {'tweet-id': "CBC", 'content': "These tweets are placeholders because there is no backend yet as of this pull request!", 'keywords': ["hello","universe"]}];
-      let componentContext = this;
+      let cmp = this
       axios.get('http://localhost:5000/search/' + term)
         .then(function (response) {
-          componentContext.$data.tweetList = response.data;
+          cmp.tweetList.push.apply(cmp.tweetList, response.data.slice(cmp.startIndex, cmp.endIndex));
+          cmp.startIndex += cmp.newTweetFactor;
+          cmp.endIndex += cmp.newTweetFactor;
         })
         .catch(function (error) {
           console.log(error);
+          cmp.tweetList = [];
        });
+    },
+    scroll(tweetList){
+      let cmp = this
+      window.onscroll = ()  => {
+        let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+
+        if (bottomOfWindow) {
+          axios.get('http://localhost:5000/search/' + cmp.searchText)
+            .then(function (response) {
+              cmp.tweetList.push.apply(cmp.tweetList, response.data.slice(cmp.startIndex, cmp.endIndex));
+              cmp.startIndex += cmp.newTweetFactor;
+              cmp.endIndex += cmp.newTweetFactor;
+            })
+            .catch(function (error) {
+              console.log(error);
+              cmp.tweetList = [];
+           });
+        }
+      }
     }
   },
   components: {
     'tweet-list': TweetList
+  },
+  watch: {
+    searchText: function () {
+      this.tweetList = [];
+      this.startIndex = 0;
+      this.endIndex = 9;
+    }
+  },
+  mounted() {
+    this.scroll(this.tweetList);
   }
 }
 </script>
