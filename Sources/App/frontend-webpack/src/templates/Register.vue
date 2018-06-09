db199821d8233a8074d50ca32e429ea85d6f1268
Jean P.D. Meijer <donovan.meijer@hotmail.com>
2018-06-09 13:41:21 +0200

    Added registration page.

--- Sources/App/frontend-webpack/src/templates/Register.vue
+++ Sources/App/frontend-webpack/src/templates/Register.vue
@@ -1,5 +1,53 @@
 <template>
-    <nav-bar></nav-bar>
+    <div>
+        <nav-bar></nav-bar>
+        <div class="d-flex justify-content-center" style="margin-top: 100px;">
+            <div class="card" style="width: 500px; color: black !important;">
+                <header class="card-header">
+                    <a href="" class="float-right btn btn-outline-primary mt-1">Log in</a>
+                    <h4 class="card-title mt-2">Sign up</h4>
+                </header>
+                <article class="card-body">
+                    <form>
+                        <div class="form-row">
+                            <div class="col form-group">
+                                <label>First name</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div> <!-- form-group end.// -->
+                            <div class="col form-group">
+                                <label>Last name</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div> <!-- form-group end.// -->
+                        </div> <!-- form-row end.// -->
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control" placeholder="">
+                            <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div> <!-- form-group end.// -->
+                        <div class="form-group">
+                            <label class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="gender" value="option1">
+                                <span class="form-check-label"> Male </span>
+                            </label>
+                            <label class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="gender" value="option2">
+                                <span class="form-check-label"> Female</span>
+                            </label>
+                        </div> <!-- form-group end.// -->
+                        <div class="form-group">
+                            <label>Create password</label>
+                            <input class="form-control" type="password">
+                        </div> <!-- form-group end.// -->
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-block"> Register  </button>
+                        </div> <!-- form-group// -->
+                        <small class="text-muted">By clicking the 'Sign Up' button, you confirm that you accept our <br> Terms of use and Privacy Policy.</small>
+                    </form>
+                </article> <!-- card-body end .// -->
+                <div class="border-top card-body text-center">Have an account? <a href="">Log In</a></div>
+            </div> <!-- card.// -->
+        </div>
+    </div>
 </template>
 
 <script>
